cc3d64e33f4d8fec015e2d30756d0d0dbc020b76
Steen Johannessen <steenjohannessen@Mac-mini-tilhrende-Steen.local>
2026-07-24 07:07:45 +0200

    fix: tillad lokal login via Tailscale

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["canvas"],
+  allowedDevOrigins: ["100.112.99.59"],
 };
 
 export default nextConfig;
